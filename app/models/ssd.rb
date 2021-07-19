8d7969f9b32757250bc7f2d6b464c7c6d51f5f06
Jaren Cudilla <jarencudilla@gmail.com>
2021-07-19 22:26:36 +0800

    Updated ssd model so we can scrape data for the ssd table

--- app/models/ssd.rb
+++ app/models/ssd.rb
@@ -1,2 +1,46 @@
 class Ssd < ApplicationRecord
+  has_many :build_ssd, dependent: :destroy
+  has_many :builds, through: :build_ssds
+
+  def self.name
+    @name = 'ssds'
+  end
+
+  def self.url
+    @start_urls = ["https://pangoly.com/en/browse/ssd?page=#{@page}"]
+  end
+
+  def self.parse_repo_page(response)
+    item = {}
+    @table = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody")
+    item[:capacity] = get_text('Capacity')
+    item[:nand_flash] = get_text('NAND Flash Type')
+    item[:form_factor] = get_text('Form Factor')
+    item[:interface] = get_text('Interface')
+    item[:price] = response.xpath("//span[@class='price']").text.tr('^0-9.', '').to_f
+    item[:name] = response.xpath("//div[contains(@class, 'product-info')]").css('h2').text
+    item[:image] = response.xpath("//img[@class='nivo-main-image']").attr('src').value
+    find_or_create_by(item)
+  end
+
+  def self.get_text(column)
+    info = @table.css('tr').xpath("//strong[text()='#{column}']").xpath('..').xpath('..').css('td')[1]
+    return nil unless info
+
+    info.text.squish
+  end
+
+  def self.get_int(column)
+    info = @table.css('tr').xpath("//strong[text()='#{column}']").xpath('..').xpath('..').css('td')[1]
+    return nil unless info
+
+    info.text.squish.to_i
+  end
+
+  def self.get_arr(column)
+    info = @table.css('tr').xpath("//strong[text()='#{column}']").xpath('..').xpath('..').css('td')[1]
+    return nil unless info
+
+    info.css('ul/li').map(&:text)
+  end
 end

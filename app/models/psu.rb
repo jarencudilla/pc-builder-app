26ee11ebbe137ce03e6d2ef8ac2ed3ae9e5f6740
AdrianIlaga <adrianilaga771@gmail.com>
2021-07-19 16:45:30 +0800

    added Psu scraper function

--- app/models/psu.rb
+++ app/models/psu.rb
@@ -1,3 +1,46 @@
 class Psu < ApplicationRecord
   has_many :builds, dependent: :destroy
+
+  def self.name
+    @name = 'psus'
+  end
+
+  def self.url
+    @start_urls = ["https://pangoly.com/en/browse/psu?page=#{@page}"]
+  end
+
+  def self.parse_repo_page(response)
+    item = {}
+    @table = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody")
+    item[:form_factor] = get_text('Type')
+    item[:efficiency_rating] = get_text('Efficiency')
+    item[:wattage] = get_text('Wattage')
+    item[:modular] = get_text('Modular')
+    item[:price] = response.xpath("//span[@class='price']").text.tr('^0-9.', '').to_f
+    item[:name] = response.xpath("//div[contains(@class, 'product-info')]").css('h2').text
+    item[:image] = response.xpath("//img[@class='nivo-main-image']").attr('src').value
+    byebug
+    find_or_create_by(item)
+  end
+
+  def self.get_text(column)
+    info = @table.css('tr').xpath("//strong[text()='#{column}']").xpath('..').xpath('..').css('td')[1]
+    return nil unless info
+
+    info.text.squish
+  end
+
+  def self.get_int(column)
+    info = @table.css('tr').xpath("//strong[text()='#{column}']").xpath('..').xpath('..').css('td')[1]
+    return nil unless info
+
+    info.text.squish.to_i
+  end
+
+  def self.get_arr(column)
+    info = @table.css('tr').xpath("//strong[text()='#{column}']").xpath('..').xpath('..').css('td')[1]
+    return nil unless info
+
+    info.css('ul/li').map(&:text)
+  end
 end

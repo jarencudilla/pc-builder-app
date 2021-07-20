31acce06fa648e27ed64cf367743f9dcfcf12b1e
jarencudilla <jarencudilla@gmail.com>
2021-07-21 00:15:02 +0800

    [WIP] Initial View for the system builder

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -2,6 +2,41 @@ class BuildsController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    @builds = Build.all
+    @cpus = Cpu.all
+    @mobos = Mobo.all
+  end
+
+  def show
+    @cpus = Cpu.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @cpus }
+    end
+
+    @mobos = Mobo.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @mobos }
+    end
+  end
+
+  def new
+    @build = Build.new
+  end
+
+  def edit; end
+
+  def create; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def product_params
+    params.require(:build).permit(:name, :purpose, :total_price)
   end
 end

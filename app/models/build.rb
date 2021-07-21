c00861ac2f1e2a20703f58f9b793cf84c8f5966b
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2021-07-21 19:45:06 +0800

    corrected build ssds rel. corrected nested attr.

--- app/models/build.rb
+++ app/models/build.rb
@@ -12,14 +12,14 @@ class Build < ApplicationRecord
   has_many :rams, through: :build_rams
   has_many :build_case_fans, dependent: :destroy
   has_many :case_fans, through: :build_case_fans
-  has_many :build_ssd, dependent: :destroy
+  has_many :build_ssds, dependent: :destroy
   has_many :ssds, through: :build_ssds
   has_many :build_hdds, dependent: :destroy
   has_many :hdds, through: :build_hdds
 
-  accepts_nested_attributes_for :gpus
-  accepts_nested_attributes_for :rams
-  accepts_nested_attributes_for :case_fans
-  accepts_nested_attributes_for :ssds
-  accepts_nested_attributes_for :hdds
+  accepts_nested_attributes_for :build_gpus
+  accepts_nested_attributes_for :build_rams
+  accepts_nested_attributes_for :build_case_fans
+  accepts_nested_attributes_for :build_ssds
+  accepts_nested_attributes_for :build_hdds
 end

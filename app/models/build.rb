fa3089f115502362ae7d8a4ba3849df924b1ad81
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2021-07-21 15:54:11 +0800

    Removed strages, corrected ssd and hdd, add nests

--- app/models/build.rb
+++ app/models/build.rb
@@ -5,16 +5,21 @@ class Build < ApplicationRecord
   belongs_to :psus
   belongs_to :cases
   belongs_to :cpu_fans
+
   has_many :build_gpus, dependent: :destroy
   has_many :gpus, through: :build_gpus
   has_many :build_rams, dependent: :destroy
   has_many :rams, through: :build_rams
-  has_many :build_storages, dependent: :destroy
-  has_many :storages, through: :build_storages
   has_many :build_case_fans, dependent: :destroy
   has_many :case_fans, through: :build_case_fans
   has_many :build_ssd, dependent: :destroy
-  has_many :builds, through: :build_ssds
+  has_many :ssds, through: :build_ssds
   has_many :build_hdds, dependent: :destroy
-  has_many :builds, through: :build_hdds
+  has_many :hdds, through: :build_hdds
+
+  accepts_nested_attributes_for :gpus
+  accepts_nested_attributes_for :rams
+  accepts_nested_attributes_for :case_fans
+  accepts_nested_attributes_for :ssds
+  accepts_nested_attributes_for :hdds
 end

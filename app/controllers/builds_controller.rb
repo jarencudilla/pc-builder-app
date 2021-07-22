29b9d86c32e67afd5b003771cb83a0b9d48bcfd4
EliezerAmbion <eliezerambion@gmail.com>
2021-07-22 15:36:47 +0800

    created partials for hardwares

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,7 +1,48 @@
 class BuildsController < ApplicationController
   # before_action :authenticate_user!
 
-  def index
-    @builds = Build.all
+  # def index
+  #   @builds = Build.all
+  # end
+
+  def cpu
+    @cpus = Cpu.all
   end
+
+  def mobo
+    @mobos = Mobo.all
+  end
+
+  def case
+    @cases = Cases.all
+  end
+
+  def gpu
+    @gpus = Gpu.all
+  end
+
+  def hdd
+    @hdds = Hdd.all
+  end
+
+  def psu
+    @psus = Psu.all
+  end
+
+  def ram
+    @rams = Ram.all
+  end
+
+  def ssd
+    @ssds = Ssd.all
+  end
+
+  def case_fan
+    @case_fans = Case_fan.all
+  end
+
+  def cpu_fan
+    @cpu_fans = Cpu_fan.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
 
-  resources :builds
+  # resources :builds
+
+  get 'builds/cpu' => 'builds#cpu', as: 'cpu'
+  get 'builds/mobo' => 'builds#mobo', as: 'mobo'
+  get 'builds/case' => 'builds#case', as: 'case'
+  get 'builds/gpu' => 'builds#gpu', as: 'gpu'
+  get 'builds/hdd' => 'builds#hdd', as: 'hdd'
+  get 'builds/psu' => 'builds#psu', as: 'psu'
+  get 'builds/ram' => 'builds#ram', as: 'ram'
+  get 'builds/ssd' => 'builds#ssd', as: 'ssd'
+  get 'builds/case_fan' => 'builds#case_fan', as: 'case_fan'
+  get 'builds/cpu_fan' => 'builds#cpu_fan', as: 'cpu_fan'
 end

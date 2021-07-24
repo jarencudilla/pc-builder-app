3ece38f3223c34dcf03ee33de2dddb30d159ee53
AdrianIlaga <adrianilaga771@gmail.com>
2021-07-24 09:36:57 +0800

    fixed new build form

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class BuildsController < ApplicationController
   # before_action :authenticate_user!
 
@@ -33,10 +34,10 @@ class BuildsController < ApplicationController
   def new
     @build = Build.new
     @build_case_fans = @build.build_case_fans.build
-    @build_gpu = @build.build_gpus.build
-    @build_hdd = @build.build_hdds.build
-    @build_ram = @build.build_rams.build
-    @build_ssd = @build.build_ssds.build
+    @build_gpus = @build.build_gpus.build
+    @build_hdds = @build.build_hdds.build
+    @build_rams = @build.build_rams.build
+    @build_ssds = @build.build_ssds.build
 
     #computer parts#
     @cpu = Cpu.find(params[:cpu_id])
@@ -53,6 +54,7 @@ class BuildsController < ApplicationController
 
   def create
     @build = Build.new(build_params)
+    
 
     if @build.save
       redirect_to root_path, notice: 'Build Successfully Created'
@@ -70,16 +72,11 @@ class BuildsController < ApplicationController
   private
 
   def build_params
-    params.require(:build).permit(:name, :purpose, :user_id, :cpu_id, :mobo_id, :psu_id, :case_id, :cpu_fan_id,
-                                  build_cpus_attributes: %i[id cpu_id],
-                                  build_mobos_attributes: %i[id mobo_id],
-                                  build_cpu_fans_attributes: %i[id cpu_fan_id],
+    params.require(:build).permit(:name, :purpose, :user_id, :cpu_id, :mobo_id, :psu_id, :case_id, :cpu_fan_id, :total_price,
                                   build_gpus_attributes: %i[id gpu_id],
                                   build_rams_attributes: %i[id ram_id],
                                   build_hdds_attributes: %i[id hdd_id],
                                   build_ssds_attributes: %i[id ssd_id],
-                                  build_cases_attributes: %i[id case_id],
-                                  build_psus_attributes: %i[id psu_id],
                                   build_case_fans_attributes: %i[id case_fan_id])
   end
 end
